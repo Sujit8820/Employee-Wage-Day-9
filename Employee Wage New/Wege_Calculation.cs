0444c2b83086443114c5cb3e4a7bbc0ba7e58a2c
Sujit8820 <kadamsujit8820@gmail.com>
2022-06-15 20:59:03 +0530

    [Sujit]Add: Add Part time wage also

--- Employee Wage New/Wege_Calculation.cs
+++ Employee Wage New/Wege_Calculation.cs
@@ -12,30 +12,37 @@ namespace Employee_Wage_New
 
             public void EmpWage()
             {
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
 
-                int workingHrs = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
 
-
-                //Constants
-                const int ABSENT = 0;
-                const int WAGEPERHRS = 20;
+            int workingHrs = 0;
 
 
-                if (empCheck == ABSENT)
-                {
-                    workingHrs = 0;
-                    Console.WriteLine("Employee is Absent");
-                }
-                else
-                {
-                    workingHrs = 8;
-                    Console.WriteLine("Employee is Present");
-                }
-                int empWage = WAGEPERHRS * workingHrs;
-                
-                Console.WriteLine("Employee Wage"+ empWage);
+            //Constants
+            const int ABSENT = 0;
+            const int FULLTIME = 1;
+            const int WAGEPERHRS = 20;
+
+
+            if (empCheck == ABSENT)
+            {
+                workingHrs = 0;
+                Console.WriteLine("Employee is Absent");
+            }
+            else if (empCheck == FULLTIME)
+            {
+                workingHrs = 8;
+                Console.WriteLine("Employee is full time present");
+            }
+            else
+            {
+                workingHrs = 4;
+                Console.WriteLine("Employee is part time present");
+            }
+            int empWage = WAGEPERHRS * workingHrs;
+
+            Console.WriteLine("Employee Wage "+ empWage);
             }
         
     }
